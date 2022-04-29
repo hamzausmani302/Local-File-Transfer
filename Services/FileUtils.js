9296e1fd92fc03ebbf1e83dc6028c5d04bfd5cec
hamzausmani302 <hamzausmani021@gmail.com>
2022-04-29 04:52:23 -0700

    1.login 2.Sockets 3.List Files

--- Services/FileUtils.js
+++ Services/FileUtils.js
@@ -1,7 +1,25 @@
+const fs = require("fs")
+const path =require("path");
+
+const root_path = path.resolve(__dirname , "../SharedDirectory")
+
+const result = {error : 0}; 
 class FileUtils{
 
     static ListFiles = ()=>{
-
+        let promise = new Promise((resolve , reject)=>{
+            fs.readdir(root_path, (err , files)=>{
+                if(err){
+                    result.error = 1 
+                    result.msg = err.message
+                    reject(err.message)  
+                }
+                resolve(files)
+                return files;
+    
+            })
+        });
+        return promise;
     }
 
     static MakeDirectory = ()=>{
--- index.js
+++ index.js
@@ -1,26 +1,58 @@
+const express = require("express");
+let ejs = require('ejs');
+const FileUtils = require("./Services/FileUtils");
 
-// start server make routes
-
-
-const {IPUTILS}=  require(__dirname + "/Services/IPUTILS");
-const dotenv = require("dotenv");
-const express= require("express")
-const App = express()
-const IP_ADDR = IPUTILS.getintefaceIP()
-
-
-App.get("/" , (req , res)=>{
-    const ip = IPUTILS.getintefaceIP();
-    res.send(ip);
-})
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+app.set("view-engine" , 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
 
 
-App.listen( 3000 , ()=>{
-    console.log("connected");
+app.get('/', (req, res) =>{
+   res.render(__dirname + "/View/index.ejs" , {error:""});
 });
 
+app.post("/login" , (req,res)=>{
+    const {username , password} = req.body;
+    if(username=="fast" && password == "fast"){
+        FileUtils.ListFiles()
+        
+        .then((data)=>{
+            console.log("data",data);
+            res.render(__dirname + "/View/Home.ejs" ,{
+                content : JSON.stringify(data)
+            } )
+            return ;
+        }).catch((err)=>{
+            res.send(err.message)
+        });
+        
+    
+       return;
+    }
+    res.render(__dirname + "/View/index.ejs" ,{
+        error : "Invlaid username and password"
+    } )
 
+    // console.log(`Login with username = ${req.body.username} password= ${req.body.password} `)
+})
 
+//Whenever someone connects this gets executed
+io.on('connection', function(socket) {
+   console.log('A user connected' );
+   
 
+   //Whenever someone disconnects this piece of code executed
+   socket.on('disconnect', function () {
+      console.log('A user disconnected');
+   });
+   socket.on("message" , function(message){
+       console.log(message);
+   })
+});
 
-
+http.listen(3000, function() {
+   console.log('listening on *:3000');
+});
